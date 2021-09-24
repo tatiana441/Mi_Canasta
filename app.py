5bece8c03489bad35ee1fa1982b99f541794c464
Kevin Alexis Rondon Gonzalez <85571720+Kevin-Alexis-Rondon-Gonzalez@users.noreply.github.com>
2021-09-24 16:38:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 import usuarios
 usuariosVisitante = usuarios.Usuarios("sandra","123456789","sandra@")
 usuariosVisitante.delete()
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
