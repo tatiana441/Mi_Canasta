67c70bd26f2031ba99035f2a715396ca77b8e0ce
Kevin Alexis Rondon Gonzalez <85571720+Kevin-Alexis-Rondon-Gonzalez@users.noreply.github.com>
2021-09-24 18:28:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,3 @@ usuariosVisitante = usuarios.Usuarios("kevin")
 usuariosVisitante.get_nombre()
 
 print("")
-=======
-usuariosVisitante = usuarios.Usuarios("sandra","123456789","sandra@")
-usuariosVisitante.delete()
->>>>>>> 52745f995ae5f7ca0a02a92fd1eb5855c3fd27b1
